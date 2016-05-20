b53a53afa520258052e1e18792837a2295e434f3
Mateusz Kupilas <mateusz@kupilas.pl>
2016-05-20 19:32:58 +0200

    Made Song1 class

--- src/pl/javadevmatt/jmusicplayground/ConsoleLauncher.java
+++ src/pl/javadevmatt/jmusicplayground/ConsoleLauncher.java
@@ -1,26 +1,15 @@
 package pl.javadevmatt.jmusicplayground;
 
-import jm.JMC;
-import jm.music.data.Note;
-import jm.music.data.Phrase;
-import jm.util.Play;
-import jm.util.View;
+import pl.javadevmatt.jmusicplayground.songs.Song1;
+
 
 public class ConsoleLauncher {
 
 	public static void main(String[] args) {
 		
-		 Note n = new Note(JMC.C4, JMC.QUARTER_NOTE);
-		 Note n2 = new Note(JMC.D4, JMC.QUARTER_NOTE);
-		 Note n3 = new Note(JMC.E4, JMC.QUARTER_NOTE);
-		 
-	     Phrase phr = new Phrase();
-	     phr.addNote(n);
-	     phr.addNote(n2);
-	     phr.addNote(n3);
-	        
-	     View.notate(phr);
-	     Play.midi(phr);
+		Song1 song = new Song1();
+		song.showNotes();
+		song.play();
 	     
 	}
 
---
+++ src/pl/javadevmatt/jmusicplayground/songs/Song1.java
@@ -0,0 +1,35 @@
+package pl.javadevmatt.jmusicplayground.songs;
+
+import jm.JMC;
+import jm.music.data.Note;
+import jm.music.data.Phrase;
+import jm.util.Play;
+import jm.util.View;
+
+public class Song1 {
+	
+	private Phrase phrase;
+
+	public Song1(){
+		init();
+	}
+
+	private void init() {
+		 Note n = new Note(JMC.C4, JMC.QUARTER_NOTE);
+		 Note n2 = new Note(JMC.D4, JMC.QUARTER_NOTE);
+		 Note n3 = new Note(JMC.E4, JMC.QUARTER_NOTE);
+		 
+		 phrase = new Phrase();
+		 phrase.addNote(n);
+		 phrase.addNote(n2);
+		 phrase.addNote(n3);
+	}
+	
+	public void showNotes(){
+		View.notate(phrase);
+	}
+	
+	public void play(){
+		 Play.midi(phrase);
+	}
+}
